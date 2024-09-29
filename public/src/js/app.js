4540dd05e9efee842e8983f0eaacb2e5e4b230d8
javad <javadyousefi9000@gmail.com>
2024-09-29 13:08:13 +0330

    add cm

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,5 @@
 if ("serviceWorker" in navigator) {
+    // scope based on the routes your want be pwa
     navigator.serviceWorker.register("/sw.js", { scope: "/" }).then((res) => {
         console.log("serviceWorkder lunched", res)
     }).catch(err => {
