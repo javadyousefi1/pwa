548ee87ef813940782da0a768b3288d5d4d7ed24
javad <javadyousefi9000@gmail.com>
2024-09-29 13:05:59 +0330

    add service worker in app js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,1 +1,7 @@
-// navigator.serviceWorker.register("/sw.js")
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").then((res) => {
+        console.log("serviceWorkder lunched",res)
+    }).catch(err => {
+        console.log("serviceWorker crashed",err)
+    })
+}
