27addb2eb6e84da78653e3a3786441e076c86db7
javad <javadyousefi9000@gmail.com>
2024-09-29 15:50:27 +0330

    sw

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+// self for accessibility to sw
+// you dont have dom access in sw
+self.addEventListener("install", (e) => {
+  console.log("install", e)
+})
+
+self.addEventListener("activate", (e) => {
+  // must close all tabs up to activated
+  console.log("active", e)
+  return self.clients.cliam()
+}) 
+
+self.addEventListener("fetch" , (e) => {
+  console.log("fetch",e)
+  e.respondWith(fetch(e.request))
+})
